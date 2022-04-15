56e31dbbdcaab5f52a23c542105aecb7ccf5efa4
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-04-15 10:27:47 +0200

    add ex02

---
+++ ex02/Array.hpp
@@ -0,0 +1,35 @@
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+#include <cstddef>
+#include <iostream>
+
+template <typename T> class Array {
+	public:
+		Array() : _size(0), _data(NULL) {}
+		Array(unsigned int n) : _size(n), _data(new T[n]()) {}
+		~Array() {
+			delete[] _data;
+		}
+		Array(const Array &array) : _size(array._size), _data(new T[_size]) {
+			std::copy(array._data, array._data + array._size, _data);
+		}
+		Array operator=(const Array &array) {
+			_size = array._size;
+			delete[] _data;
+			_data = new T[_size];
+			std::copy(array._data, array._data + array._size, _data);
+		}
+		T &operator[](unsigned int idx) {
+			if (idx < 0 || idx >= _size)
+				throw std::exception();
+			return _data[idx];
+		}
+		unsigned int size() { return _size;}
+	private:
+		unsigned int _size;
+		T *_data;
+};
+
+
+#endif
---
+++ ex02/main.cpp
@@ -0,0 +1,33 @@
+
+#include "Array.hpp"
+#include <iostream>
+
+int main() {
+	Array<int> arr1;
+	Array<std::string> arr2(3);
+	Array<int> arr3(5);
+
+	std::cout << arr3[2] << std::endl;
+	try {
+		std::cout << arr3[-1] << std::endl;
+	} catch (std::exception &e) {(void) e;}
+	try {
+		std::cout << arr3[10] << std::endl;
+	} catch (std::exception &e) {(void) e;}
+
+
+	std::cout << arr3.size() << std::endl;
+
+
+	arr3[1] = 10;
+	Array<int> arr4 = arr3;
+	arr4[2] = 20;
+
+	std::cout << "arr 3: " << std::endl;
+	for (unsigned int i = 0; i < arr3.size(); i++)
+		std::cout << arr3[i] << " ";
+	std::cout << "\narr 4: " << std::endl;
+	for (unsigned int i = 0; i < arr4.size(); i++)
+		std::cout << arr4[i] << " ";
+	std::cout << std::endl;
+}
